0045027d2dc550f19e2423d682b0d9942d44a568
Nazdorovye <naz-x@naz-xhm.zyxel.com>
2020-10-11 17:38:13 +0300

    Added Task0102.java . Moved workspace file.

---
+++ Task0102.java
@@ -0,0 +1,33 @@
+/*
+ * This file is a part of solutions for the Java Basics course in the
+ *      Ventspils University of Applied Sciences.
+ *  There is an attempt to maintain Google Java code style declared at
+ *      https://google.github.io/styleguide/javaguide.html
+ *  
+ *  Author: Stanislavs Vjazovcevs
+ *  Email:  cheshirebarsyk@gmail.com, s20vjazstan@venta.lv
+ *
+ *  Copyright Vjazovcevs Stanislavs, 2020
+ */
+
+/**
+ * This class is associated with the second task of the first seminar
+ */
+
+public class Task0102 {
+  public static void main(String[] args) {
+    int a = 5;
+    float b = 9.99f;
+    boolean c = true;
+    char d = 'A';
+    byte e = 127;
+    byte f = (byte)129;
+    char g = 65;
+    short h = 300;
+    byte i = (byte)h;
+    long j = Long.MAX_VALUE;
+
+    System.out.printf("a = %d\nb = %.2f\nc = %b\nd = %c\ne = %d\nf = %d\ng = %c\ni = %d\nj = %d\n",
+        a, b, c, d, e, f, g, i, j);
+  }
+}
--- task_01_02.java
+++
@@ -1,5 +0,0 @@
-public class task_01_02 {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
-}
